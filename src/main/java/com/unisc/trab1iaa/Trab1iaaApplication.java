41d716ec811b8faad4df17a5f2e08a84337c2fe7
Anderson Rodrigues <maikbru@gmail.com>
2022-10-04 17:16:00 -0300

    first

---
+++ src/main/java/com/unisc/trab1iaa/Trab1iaaApplication.java
@@ -0,0 +1,13 @@
+package com.unisc.trab1iaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trab1iaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trab1iaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisc/trab1iaa/Trab1iaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisc.trab1iaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trab1iaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
